217459312e4a2741267d86a4bb6987ee115ffb84
KirDuch <201406768@post.au.dk>
2018-09-13 19:48:37 +0200

    Illustration og data, færdig version

--- projekt.R
+++ projekt.R
@@ -1,103 +1,58 @@
 # Read data
 Dat <- as.matrix(read.table("Data.R", sep="",head=TRUE,fill = TRUE))
 #Formate data as matrix
-Dat_new <- matrix(as.numeric(Dat[,-1]),ncol=4,nrow=29)
-rownames(Dat_new) <- Dat[,1]
-n_est <- sum(Dat_new,na.rm = TRUE)/4
+Dat_new <- na.omit(matrix(as.numeric(Dat[,-1]),ncol=4,nrow=29))
+rownames(Dat_new) <- Dat[-c(28,29),1]
 
-#------------------------
-#Lidt overvejelser stadig grim kode
-#----------------
+# Sample size
+n <- colSums(Dat_new)
+n_mean <- sum(Dat_new)/4
 
-#calculate mean
-mean <- apply(Dat_new,1,mean)
 # Plot data
-df <- barplot(t(na.omit(Dat_new)),beside=TRUE,col = rainbow(4))
-lines(df,rep(na.omit(mean),each=4),lty=2,lwd=1)
-lines(df,rep(mean(na.omit(mean)),each=length(df)),lty=2,col="red")
+mean <- apply(Dat_new,1,mean)
+df <- barplot(t(Dat_new),beside=TRUE,col = rainbow(4))
+lines(df,rep(mean,each=4),lty=2,lwd=1)
+lines(df,rep(mean(mean),each=length(df)),lty=2,col="red")
 legend("topleft",c("L. Mean","T. Mean"),lty = 2,col=c("black","red"))
 
 
-#X vokal vs. konsonant 
-z <- ifelse(rownames(Dat_new)%in% c("A","E","I",
+# X vokal vs. konsonant 
+id <- ifelse(rownames(Dat_new)%in% c("A","E","I",
                                     "O","U","Y",
                                     "AE"), TRUE,FALSE)
-y_bar <- sum(Dat_new[z])/(n_est*4)
-y_bar
+# y_bar
+y_bar <- colSums(Dat_new[id,])/n
 
-y_bar1 <- sum(Dat_new[z,1])/sum(Dat_new[,1],na.rm = TRUE)
-y_bar2 <- sum(Dat_new[z,2])/sum(Dat_new[,2],na.rm = TRUE)
-y_bar3 <- sum(Dat_new[z,3])/sum(Dat_new[,3],na.rm = TRUE)
-y_bar4 <- sum(Dat_new[z,4])/sum(Dat_new[,4],na.rm = TRUE)
+#Population total if distiributed according to aplhabet
+y_v9 <- 9/29 #vowels in danish alphabet
+y_v7 <- 7/27 #expected vowels based on what we sample 
 
-y_barP <- c(y_bar1,y_bar2,y_bar3,y_bar4)
-
-v9 <- 9/29 #vowels in danish alphabet
-v7 <- 7/27 #expected vowels based on what we sample 
-
-
-# with variance estimator
-var_emp <- (1-0)*y_bar*(1-y_bar)/n_est
+# Empirical variance
+var_emp <- (1-0)*y_bar*(1-y_bar)/(n-1)
 var_emp
-var_p <- y_barP*(1-y_barP)/colSums(Dat_new,na.rm=TRUE)
 
-# with variariance based on "true" alphabet
-var_t_v9 <- 1*v9*(1-v9)/n_est
-var_t_v7 <- v7*(1-v7)/n_est
-# which is very close to the variation we see in the alphabeth. 
+# True variance
+var_t_v9 <- 29/28*y_v9*(1-y_v9)
+var_t_v7 <- 27/26*y_v7*(1-y_v7)
+c(var_t_v9,var_t_v7)
+
+# 95% -CI for the empirical variance
+uu <- qnorm(.975)
+CI <- cbind(y_bar-uu*sqrt(var_emp),y_bar+uu*sqrt(var_emp))
+# both y_v9 an y_v7 are in the CI.
+
 
 #Hyppige bogstaver i noveller
-z <- ifelse(rownames(Dat_new)%in% c("E","N","D","R"), 
+id2 <- ifelse(rownames(Dat_new)%in% c("E","N","D","R"), 
                  TRUE,FALSE)
-y_bar_endr1 <- sum(Dat_new[z,1])/sum(Dat_new[,1],na.rm = TRUE)
-y_bar_endr2 <- sum(Dat_new[z,2])/sum(Dat_new[,2],na.rm = TRUE)
-y_bar_endr3 <- sum(Dat_new[z,3])/sum(Dat_new[,3],na.rm = TRUE)
-y_bar_endr4 <- sum(Dat_new[z,4])/sum(Dat_new[,4],na.rm = TRUE)
-y_endr <- c(y_bar_endr1,y_bar_endr2,y_bar_endr3,y_bar_endr4)
+y_endr <- colSums(Dat_new[id2,])/n
+var_endr <- y_endr*(1-y_endr)/(n-1)
 
-var_endr <- y_endr*(1-y_endr)/(colSums(Dat_new,na.rm=TRUE)-1)
-round(sqrt(var_endr),4)
-round(sqrt(4/27*(1-4/27)/colSums(Dat_new,na.rm=TRUE)),4)
+CI <- cbind(y_endr-uu*sqrt(var_endr),y_endr+uu*sqrt(var_endr))
 
+# value for alphabet 
+v_29 <- 4/29
+v_27 <- 4/27
+c(v_29,v_27)
 
-
-
-
-
-
-
-
-
-# Total number of letters pr. pag
-total_b <- rowSums(Dat_new/4)
-total_ABC <- sum(total_b,na.rm=TRUE)
-
-variation <- var(t(na.omit(Dat_new)))
-sd <- sqrt(diag(variation))
-round(sd,2)
-#for each letter we expect a variation between 1 and 3 peaces within a bag
-c(max(sd),min(sd)) 
-#with the variation sortet as follows - note we have omittet letters not observed
-names(sort(sd,decreasing=TRUE)) #ovious we have the least variation in M and W.
-
-# Calculate coefficients of error expressed in percentage
-round(sd/na.omit(mean)*100,2)
-
-#If I want to be able to spell my name I would want to know how many bags of candy I needed to 
-# by to be quite sure all letters where their. 
-# Assume total populationsize is total number of letters + Assume independens
-# Is it more likely to see to different letters then to of the same, 
-#(is Simon more happy then Nanna?)
-
-# Expected inclusion probabilities based on samples
-pii <- (rowSums(Dat_new))/(sum(Dat_new,na.rm=TRUE))
-round(pii,4)
-
-
-# Det er m?rkeligt
-pij <- pii["A"]*(rowSums(Dat_new)["B"]/4)/(sum(Dat_new,na.rm = TRUE)/4-1)
-pij <- t(matrix(rep(na.omit(pii),27),ncol=27))*
-  matrix(rep(rowSums(na.omit(Dat_new))/4,27),ncol=27)/
-  (sum(Dat_new,na.rm = TRUE)/4-1)
-
-# vowel vs consonant
+# again we cannit reject that the letters occure as in the alphabet. 
